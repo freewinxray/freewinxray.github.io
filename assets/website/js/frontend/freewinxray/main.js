96bc8b99aabd6ba746d3103e0e8beb6e3858065b
sihasiz@outlook.com <sihasiz@outlook.com>
2025-02-07 09:55:13 +0800

    jin nian di er ji du iPhone zai yin du shi chang de xiao liang zeng fu da dao liang wei shu chuang xia li

--- assets/website/js/frontend/freewinxray/main.js
+++ assets/website/js/frontend/freewinxray/main.js
@@ -172,12 +172,6 @@
     });
   }
 
-  /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
 
   /**
    * Skills animation
@@ -244,12 +238,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
   /**
    * Portfolio details slider
